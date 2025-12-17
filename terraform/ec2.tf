6afdf3c1fd89cbbb764683e7f9bbf558846d8230
HARISH07-1 <m.harish0705@gmail.com>
2025-12-17 18:25:18 +0530

    jenkins aws credentials

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,38 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (Mumbai)
-  instance_type = "t2.micro"
-  key_name      = "my-key"
+# Create a key pair
+resource "aws_key_pair" "devops_key" {
+  key_name   = "devops-key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key
+}
 
+# Security group (assuming you already have this)
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Managed by Terraform"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.devops_key.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
