df73374a533d8b46b6cb934a3a35beda585448e8
HARISH07-1 <m.harish0705@gmail.com>
2025-12-23 21:56:15 +0530

    Remove duplicate security group from ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,12 @@
-resource "aws_security_group" "app_sg" {
-  name        = "app-sg"
-  description = "Allow HTTP"
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_instance" "app" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+  ]
+
+  tags = {
+    Name = "devops-mini-project"
   }
 }
