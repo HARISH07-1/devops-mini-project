2354badfd24ed8c62cf326bac7f0705ad1ccc83f
HARISH07-1 <m.harish0705@gmail.com>
2025-12-23 22:01:13 +0530

    Use free tier eligible t3.micro

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   vpc_security_group_ids = [
     aws_security_group.app_sg.id
