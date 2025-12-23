91b077c0278197ef451ddc43d94ea895e461bba2
HARISH07-1 <m.harish0705@gmail.com>
2025-12-23 21:52:23 +0530

    Fix duplicate security group issue

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,10 +1,20 @@
 resource "aws_security_group" "app_sg" {
   name        = "app-sg"
-  description = "Allow HTTP"
-  
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -14,5 +24,9 @@ resource "aws_security_group" "app_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
   }
 }
